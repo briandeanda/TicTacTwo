fd212d6daad9022b74ad6ba7cbd8f763e1bad8f6
Daniel Diaz <diazjfdaniel@gmail.com>
2016-05-06 08:51:28 -0700

    Accept and Connect threads

---
+++ app/src/main/java/com/tictactwo/daniel/brian/tictactwo/ConnectThread.java
@@ -0,0 +1,69 @@
+package com.tictactwo.daniel.brian.tictactwo;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Created by Algernon on 5/6/16.
+ */
+public class ConnectThread extends Thread{
+    private final BluetoothSocket mSocket;
+    private final BluetoothDevice mmDevice;
+    private final BluetoothAdapter mAdapter;
+    private static ConnectThread instance = null;
+    private final String LOG_TAG = "Connect";
+    private final static UUID MY_UUID = UUID.fromString("067e6162-3b6f-4ae2-a171-2470b63dff00");
+
+    protected ConnectThread(BluetoothDevice device, BluetoothAdapter adapter) {
+        BluetoothSocket tmp = null;
+        mmDevice = device;
+        mAdapter = adapter;
+        try {
+            tmp = mmDevice.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d(LOG_TAG, "Created Socket!");
+        mSocket = tmp;
+    }
+
+    public static ConnectThread getInstance(BluetoothDevice device, BluetoothAdapter adapter) {
+        if (instance == null) {
+            instance = new ConnectThread(device, adapter);
+        }
+        return instance;
+    }
+
+
+    public void run() {
+        mAdapter.cancelDiscovery();
+        Log.v(LOG_TAG, "cancel dics");
+        try {
+            mSocket.connect();
+        } catch (IOException e) {
+            Log.d(LOG_TAG, "COULDN't Connect");
+            Log.d(LOG_TAG, e.toString());
+            try {
+                mSocket.close();
+            } catch (IOException closeException) {
+                Log.d(LOG_TAG, "COULDNT CLOSE");
+                closeException.printStackTrace();
+            }
+            return;
+        }
+        Log.v(LOG_TAG, "SCORE!");
+    }
+
+    public void cancel() {
+        try {
+            mSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/tictactwo/daniel/brian/tictactwo/ListDevicesActivity.java
@@ -0,0 +1,127 @@
+package com.tictactwo.daniel.brian.tictactwo;
+
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by Algernon on 5/5/16.
+ */
+public class ListDevicesActivity extends ListActivity{
+
+    private BluetoothAdapter  mBluetoothAdapter= null;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final String LOG_TAG = "LIST";
+    private ArrayList<String> devices = new ArrayList<>(10);
+    private DevicesAdapter devicesAdapter = null;
+    private final UUID MY_UUID = UUID.fromString("067e6162-3b6f-4ae2-a171-2470b63dff00");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(ListDevicesActivity.this, "You need bluetooth to play our game ):", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT);
+        } else {
+            Log.d(LOG_TAG, "Starting discovery");
+            if (mBluetoothAdapter.isDiscovering()) {
+                Log.v(LOG_TAG, "CANCEL DISC");
+                mBluetoothAdapter.cancelDiscovery();
+            }
+            mBluetoothAdapter.startDiscovery();
+        }
+
+        devicesAdapter = new DevicesAdapter(this, devices, mBluetoothAdapter);
+
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+        setListAdapter(devicesAdapter);
+
+        AcceptThread aT = AcceptThread.getInstance(mBluetoothAdapter);
+        Thread t = new Thread(aT);
+        t.start();
+
+        ListView lv = getListView();
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(LOG_TAG, "CLICKED");
+                Log.d(LOG_TAG, devices.get(position));
+                String address = devices.get(position).split("\n")[1];
+                Log.d(LOG_TAG, address);
+                BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+                ConnectThread t = ConnectThread.getInstance(device, mBluetoothAdapter);
+                t.run();
+            }
+        });
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(LOG_TAG, action);
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                Log.d(LOG_TAG, device.getAddress());
+                devicesAdapter.add(device.getName() + "\n" + device.getAddress());
+                devicesAdapter.notifyDataSetChanged();
+                for (String d : devices) {
+                    Log.d(LOG_TAG, "Loop");
+                    Log.d(LOG_TAG, d);
+                }
+                Log.d(LOG_TAG, Integer.toString(devices.size()));
+            }
+        }
+    };
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode != RESULT_OK) {
+                Toast.makeText(ListDevicesActivity.this, "You need to activate BT!", Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                Log.d(LOG_TAG, "Starting discovery after BT");
+                if (mBluetoothAdapter.isDiscovering()) {
+                    Log.d(LOG_TAG, "CANCEL DISC");
+                    mBluetoothAdapter.cancelDiscovery();
+                }
+                mBluetoothAdapter.startDiscovery();
+            }
+        }
+    }
+}
