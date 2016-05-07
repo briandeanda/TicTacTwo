c3f50b03805d8ab27ee9db53cea1090e20a49370
Brian De Anda <briandeanda2@gmail.com>
2016-05-07 11:58:46 -0700

    connection issues

--- app/src/main/java/com/tictactwo/daniel/brian/tictactwo/DevicesAdapter.java
+++ app/src/main/java/com/tictactwo/daniel/brian/tictactwo/DevicesAdapter.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -21,37 +22,61 @@ import java.util.UUID;
 /**
  * Created by Algernon on 5/5/16.
  */
-public class DevicesAdapter extends ArrayAdapter<String> {
+public class DevicesAdapter extends BaseAdapter {
     private static final String LOG_TAG = "Adapter";
     private BluetoothAdapter adapter = null;
     private ArrayList<String> devices;
     private final Context context;
     public DevicesAdapter(Context c, ArrayList<String> devices, BluetoothAdapter bAdapter) {
-        super(c, 0, devices);
-        this.devices = new ArrayList<>(devices);
+        this.devices = devices;
         adapter = bAdapter;
         context = c;
     }
 
-    public void updateList(ArrayList<String> newDevices) {
-        devices.clear();
-        devices.addAll(newDevices);
-        this.notifyDataSetChanged();
+
+    @Override
+    public int getCount() {
+        return this.devices.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.devices.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Log.d(LOG_TAG, "Getting view");
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        String device[] = getItem(position).split("\n");
+        View v = convertView;
+        CompleteListViewHolder viewHolder;
+        if (convertView == null) {
+            LayoutInflater li = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = li.inflate(R.layout.list_layout, null);
+            viewHolder = new CompleteListViewHolder(v);
+            v.setTag(viewHolder);
+        } else {
+            viewHolder = (CompleteListViewHolder) v.getTag();
+        }
+        String device[] = this.devices.get(position).split("\n");
         Log.d(LOG_TAG, device[0]);
         Log.d(LOG_TAG, device[1]);
-        View rowView = inflater.inflate(R.layout.content_main, parent, false);
-        TextView textView = (TextView) rowView.findViewById(R.id.text);
-        textView.setText(device[0] + "\n" + device[1]);
+        Log.d(LOG_TAG, "Getting view");
 
-        return rowView;
+        viewHolder.mTvItem.setText(device[0] + "\n" + device[1]);
+
+
+        return v;
     }
+}
 
-
+class CompleteListViewHolder {
+    public TextView mTvItem;
+    public CompleteListViewHolder(View base) {
+        mTvItem = (TextView) base.findViewById(R.id.text);
+    }
 }
